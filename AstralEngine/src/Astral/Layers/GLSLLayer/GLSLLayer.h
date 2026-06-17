699d41d4f5c0bfe0a7998058b6f7168204aa1228
samprogrammation@gmail.com <samprogrammation@gmail.com>
2026-06-16 23:16:39 -0400

    shader relocation

--- AstralEngine/src/Astral/Layers/GLSLLayer/GLSLLayer.h
+++ AstralEngine/src/Astral/Layers/GLSLLayer/GLSLLayer.h
@@ -13,8 +13,8 @@ namespace Astral
 		GLSLLayer();
 		virtual void OnUpdate(const FrameContext& context) override;
 
-		const std::string VERTEX_DIR = "src/Astral/Rendering/Shaders/vertex.vert.glsl";
-		const std::string FRAGMENT_DIR = "src/Astral/Rendering/Shaders/fragment.frag.glsl";
+		const std::string VERTEX_DIR = "src/Shaders/vertex.vert.glsl";
+		const std::string FRAGMENT_DIR = "src/Shaders/fragment.frag.glsl";
 
 	private:
 		typedef uint32_t VertexBufferHandle;
